2cfe391ecd0894a8012f86eb7bb1155588091998
Kahagalle <nayomilive@gmail.com>
2019-08-01 07:49:34 +1000

    single post

--- single.php
+++ single.php
@@ -1,4 +1,12 @@
 <?php get_header(); ?>
+    <div class="nav-page-header" style="background: linear-gradient(0deg,rgba(90, 17, 125, 0.8),rgba(147, 82, 179, 0.75)), url('/img/movie-detail-bg.jpg');">
+    <div class="container clr nav-page-header-inner">
+
+        <header class="entry-header group">
+            <h1 class="entry-title" style="color: #fff"><?php the_title(); ?></h1>
+        </header>
+    </div><!-- .page-header-inner -->
+</div>
 <div class="container-inner">
     <div class="main">
         <div class="main-inner group">
@@ -7,52 +15,24 @@
                 <?php while ( have_posts() ): the_post(); ?>
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                        <div class="post-wrapper group">
-                            <div class="entry-media">
-                                <?php if( get_post_format() ) { get_template_part('inc/post-formats'); } ?>
-                            </div>
-                            <header class="entry-header group">
-                                <h1 class="entry-title"><?php the_title(); ?></h1>
-                                <div class="entry-meta">
-                                    <span class="entry-date"><i class="fa fa-calendar-o"></i><?php the_time( get_option('date_format') ); ?></span>
-                                    <?php if ( comments_open() && ( get_theme_mod( 'comment-count', 'on' ) =='on' ) ): ?>
-                                        <span class="entry-comments"><i class="fa fa-comment-o"></i><a href="<?php comments_link(); ?>"><?php comments_number( '0', '1', '%' ); ?></a></span>
-                                    <?php endif; ?>
-                                    <span class="entry-author"><i class="fa fa-user-o"></i><?php the_author_posts_link(); ?></span>
-                                    <span class="entry-category"><?php the_category(' '); ?></span>
-                                </div>
-                            </header>
-                            <div class="entry-content">
-                                <div class="entry themeform">
-                                    <?php the_content(); ?>
-                                    <?php wp_link_pages(array('before'=>'<div class="post-pages">'.esc_html__('Pages:','gridzone'),'after'=>'</div>')); ?>
-                                    <div class="clear"></div>
-                                </div><!--/.entry-->
-                            </div>
-                            <div class="entry-footer group">
-
-                                <?php the_tags('<p class="post-tags"><span>'.esc_html__('Tags:','gridzone').'</span> ','','</p>'); ?>
-
-                                <div class="clear"></div>
-
-                                <?php if ( ( get_theme_mod( 'author-bio', 'on' ) == 'on' ) && get_the_author_meta( 'description' ) ): ?>
-                                    <div class="author-bio">
-                                        <div class="bio-avatar"><?php echo get_avatar(get_the_author_meta('user_email'),'128'); ?></div>
-                                        <p class="bio-name"><?php the_author_meta('display_name'); ?></p>
-                                        <p class="bio-desc"><?php the_author_meta('description'); ?></p>
-                                        <div class="clear"></div>
-                                    </div>
-                                <?php endif; ?>
-
-                                <?php do_action( 'alx_ext_sharrre' ); ?>
-
-                                <?php if ( get_theme_mod( 'post-nav', 'content' ) == 'content' ) { get_template_part('inc/post-nav'); } ?>
-
-                                <?php if ( comments_open() || get_comments_number() ) :	comments_template( '/comments.php', true ); endif; ?>
-
-                            </div>
+                        <div class="entry-media">
+                            <?php if( get_post_format() ) { get_template_part('inc/post-formats'); } ?>
                         </div>
 
+                        <div class="entry-content">
+                            <div class="entry themeform">
+                                <?php the_content(); ?>
+                                <?php wp_link_pages(array('before'=>'<div class="post-pages">'.esc_html__('Pages:','gridzone'),'after'=>'</div>')); ?>
+                                <div class="clear"></div>
+                            </div><!--/.entry-->
+                        </div>
+                        <div class="entry-footer group">
+
+                            <?php the_tags('<p class="post-tags"><span>'.esc_html__('Tags:','gridzone').'</span> ','','</p>'); ?>
+
+                            <div class="clear"></div>
+
+                        </div>
                     </article><!--/.post-->
 
 
